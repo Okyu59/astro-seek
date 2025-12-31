3ac9ae775aa5060c18b10a45e75169ea79e177f2
Okyu59 <98271607+Okyu59@users.noreply.github.com>
2025-12-31 16:48:58 +0900

    Create vite.config.js

---
+++ frontend/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // 빌드 결과물이 저장될 폴더
+  }
+})
+

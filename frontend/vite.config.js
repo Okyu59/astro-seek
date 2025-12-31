f84cf1e6a8e8f4a1301bfc2cd618c7d824f16311
Okyu59 <98271607+Okyu59@users.noreply.github.com>
2025-12-31 17:17:36 +0900

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // 빌드 결과물이 저장될 폴더
+    outDir: 'dist',
+    emptyOutDir: true,
   }
 })
+
 
